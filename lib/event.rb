f5cf3ca5f42ed0da0609957a547979065f80551a
MelinaDCB <melina.decombas@gmail.com>
2021-10-18 13:39:24 +0200

    second-commit

--- lib/event.rb
+++ lib/event.rb
@@ -4,15 +4,47 @@ require 'time'
 class Event
     attr_accessor :start_date, :duration, :title, :attendees
 
-    def initialize(sd_to_save, duration_to_save, title_to_save, attendees_to_save)
-       @start_date = sd_to_save
-       @duration = duration_to_save
-       @title = title_to_save
-       @attendees = attendees_to_save
+    def initialize(start_date, duration, title = "", attendees = [])
+       @start_date = Time.parse(start_date)
+       @duration = duration.to_i
+       @title = title
+       @attendees = attendees
+       @@duration_in_seconds = self.duration * 60
+       @@end_time = @start_date + @@duration_in_seconds
     end
 
-    def self.postpone_24h
+    def postpone_24h
         @start_date += 86400
+        puts "the new start date is #{@start_date}."
+    end
+
+    def end_date
+        puts "The ending time is #{@@end_time}."
+    end
+
+    def is_past?
+        current_time = Time.new
+        if @start_date < current_time
+            puts "The event has already started / is probably already over."
+        else
+            puts "The event has not yet started."
+        end
+    end
+
+    def is_future?
+        current_time = Time.new
+        if @start_date > current_time
+            puts "The event has not yet started."
+        else
+            puts "The event has already started / is probably already over."
+        end
+    end
+
+    def is_soon?
+        current_time = Time.new
+        if current_time - @start_date <= 1800
+            puts "It's starting soon !"
+        end
     end
 end
 
