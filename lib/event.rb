7080e802224ffc8800c9fefe40a5f74e231365fd
MelinaDCB <melina.decombas@gmail.com>
2021-10-19 09:32:42 +0200

    initial-commit

--- lib/event.rb
+++ lib/event.rb
@@ -51,7 +51,35 @@ class Event
         puts "> Date de début : #{start_date.strftime('%m/%d/%Y %H:%M %p')} "
         puts "> Durée : #{duration} minutes "
         print "> Invités : #{attendees.join(', ')}."
+    end
+    
+    def age_analysis
+        age_array = [] #On initialise un array qui va contenir les âges de tous les participants à un évènement
+        average = 0 #On initialise une variable pour calculer la moyenne d'âge à l'évènement
+    
+        @attendees.each do |attendee| #On parcourt tous les participants (objets de type User)
+          age_array << attendee.age #leur âge est stocké dans l'array des âges
+          average = average + attendee.age #leur âge est additionné pour préparer le calcul de la moyenne
+        end
+    
+        average = average / @attendees.length #on divise la somme des âges pour avoir la moyenne
+    
+        puts "Voici les âges des participants :"
+        puts age_array.join(", ")
+        puts "La moyenne d'âge est de #{average} ans"
+    end
+end
 
+class WorkEvent < Event
+    def is_event_acceptable?
+  
+        if @attendees.length > 3 || @duration > 60
+        puts "Cette réunion ne respecte pas nos bonnes pratiques !"
+        return false
+        else
+        puts "Cette réunion est OK."
+        return true
+        end
     end
 end
 
